f6580576e7e10e74a428f54815c854b5ba170c9c
Sean Heffernan <seanheffernan@129-161-41-114.dynamic2.rpi.edu>
2022-10-11 10:12:30 -0400

    Added datasets for homework/assignments and files for Lab3

---
+++ Assignment3/DataAnalytics_A3_SEAN_HEFFERNAN.R
@@ -0,0 +1,89 @@
+nyt4 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt4.csv")
+nyt8 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt8.csv")
+nyt9 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt9.csv")
+nyt12 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt12.csv")
+nyt13 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt13.csv")
+nyt22 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt22.csv")
+nyt23 <- read.csv("Documents/Fall_2023/Data_Analytics/DataAnalyticsFall2022_Sean_Heffernan/Data_Sets/nyt23.csv")
+
+#1a
+boxplot(nyt4$Age, nyt4$Clicks)
+boxplot(nyt8$Age, nyt8$Clicks)
+boxplot(nyt9$Age, nyt9$Clicks)
+boxplot(nyt12$Age, nyt12$Clicks)
+boxplot(nyt13$Age, nyt13$Clicks)
+boxplot(nyt22$Age, nyt22$Clicks)
+boxplot(nyt23$Age, nyt23$Clicks)
+# As far as the distribution of age goes, the median seems to be around the 30 range for datasets nyt4-nyt13, and 20 for nyt22-nyt33
+# The outliers for Age tend to be above the 100 range with the upper hinge ranging between 40 and 50.
+# As for the Clicks, a since a large majority of users did not click, the median  seems to be 0, with all click counts above 0 being considered outliers.
+# The maximum number of clicks in all datasets above seems to be 4.
+
+#1b
+hist(nyt4$Impressions, col='green')
+hist(nyt4$Clicks, col='red', add=TRUE)
+hist(nyt8$Impressions, col='green')
+hist(nyt8$Clicks, col='red', add=TRUE)
+hist(nyt9$Impressions, col='green')
+hist(nyt9$Clicks, col='red', add=TRUE)
+hist(nyt12$Impressions, col='green')
+hist(nyt12$Clicks, col='red', add=TRUE)
+hist(nyt13$Impressions, col='green')
+hist(nyt13$Clicks, col='red', add=TRUE)
+hist(nyt22$Impressions, col='green')
+hist(nyt22$Clicks, col='red', add=TRUE)
+hist(nyt23$Impressions, col='green')
+hist(nyt23$Clicks, col='red', add=TRUE)
+# The impressions of the datasets seems to follow a normal distribution with a slight skew to the right with a median around 5
+# A slight skew to the right indicates a majority of users tend to have 5 or less impressions compared to 5 or more.
+# The clicks have an extremely positive skewed distribution (as the top of the histogram is the left-most side of the histogram)
+# The distribution of clicks is also far lower, with almost all clicks being either 0 or 1.
+
+#1c
+plot(ecdf(nyt4$Impressions), col='green')
+plot(ecdf(nyt4$Clicks), col='red', add=TRUE)
+qqplot(nyt4$Impressions, nyt4$Clicks)
+plot(ecdf(nyt8$Impressions), col='green')
+plot(ecdf(nyt8$Clicks), col='red', add=TRUE)
+qqplot(nyt8$Impressions, nyt8$Clicks)
+plot(ecdf(nyt9$Impressions), col='green')
+plot(ecdf(nyt9$Clicks), col='red', add=TRUE)
+qqplot(nyt9$Impressions, nyt9$Clicks)
+plot(ecdf(nyt12$Impressions), col='green')
+plot(ecdf(nyt12$Clicks), col='red', add=TRUE)
+qqplot(nyt12$Impressions, nyt12$Clicks)
+plot(ecdf(nyt13$Impressions), col='green')
+plot(ecdf(nyt13$Clicks), col='red', add=TRUE)
+qqplot(nyt13$Impressions, nyt13$Clicks)
+plot(ecdf(nyt22$Impressions), col='green')
+plot(ecdf(nyt22$Clicks), col='red', add=TRUE)
+qqplot(nyt22$Impressions, nyt22$Clicks)
+plot(ecdf(nyt23$Impressions), col='green')
+plot(ecdf(nyt23$Clicks), col='red', add=TRUE)
+qqplot(nyt23$Impressions, nyt23$Clicks)
+
+# Most of these plots tell the same stories as the previous histogram/boxplots. As you can see, about 90% of all
+# data points have clicks of 0, with about 99.98% of total clicks being beelow the 4 mark. As for impressions, 
+# the distribution is a bit more normal, with 50% of data points having around 5 or less clicks.
+# Looking at the qq plots, it looks as if again the impressions and clicks have somewhat of a right skew, with the
+# points starting at 0 clicks as the impressions go up for some time before, jumping up as the impressions cross the 8 mark.
+
+# Running shapiro test to test if Clicks/Impressions are normally distributed
+shapiro.test(nyt4$Clicks[0:5000])
+shapiro.test(nyt4$Impressions[0:5000])
+# Reject the null hypothesis as both P-values are < 0.05, meaning Clicks and Impressions are not normally distributed.
+
+# Running the Wilcoxon test to see if the two data samples are independent or not.
+wilcox.test(nyt4$Clicks, nyt4$Impressions, data=nyt4)
+# As the p-value is less than 0.05, we reject the null hypothesis, meaning the two data samples are independent.
+
+#1e
+# Generally, I noticed some of the features in the dataset being somewhat unhelpful, for example the Clicks feature.
+# Given how little times people actually "Clicked" it was hard to discover any correlations or patterns
+
+#2
+
+
+
+
+
