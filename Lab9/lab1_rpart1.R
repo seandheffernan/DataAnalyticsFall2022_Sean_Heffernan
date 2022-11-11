72c8ecf45c33c6c56c20d7c09fd2ab2982f50898
Sean Heffernan <seanheffernan@129-161-40-132.dynamic2.rpi.edu>
2022-11-11 11:25:54 -0500

    Added lab9 work

---
+++ Lab9/lab1_rpart1.R
@@ -0,0 +1,6 @@
+require(rpart)
+Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(Swiss_rpart) # try some different plot options
+text(Swiss_rpart) # try some different text options
+
+# Played around w/ a couple different options for plot and text
