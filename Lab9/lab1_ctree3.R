957c5deaa6f021489254e798a02c73a12c03f4dc
Sean Heffernan <seanheffernan@129-161-40-132.dynamic2.rpi.edu>
2022-11-11 11:48:41 -0500

    Added more lab9 work

---
+++ Lab9/lab1_ctree3.R
@@ -0,0 +1,7 @@
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosis")
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+#etc.
+
+# Unsure what else to do, but ran the code and it works
